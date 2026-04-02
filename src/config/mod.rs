ea0845fe2e3b8dd3e46a50cf4eab17225d89ef52
AVR-GCC <ogoun.d@gmail.com>
2026-04-02 22:40:20 +0300

    Format mod

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,14 +1,14 @@
 use color_eyre::Result;
 use eyre::WrapErr;
 //use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-use serde::Deserialize;
 use dotenv::dotenv;
+use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
 pub struct Config {
     pub database_url: String,
     pub host: String,
-    pub port: i32
+    pub port: i32,
 }
 //Result<Config>
 impl Config {
@@ -20,11 +20,18 @@ impl Config {
             .build()
             .context("Error: Failed to read env")?;
         let host = env.get("host").context("Error: Host not found")?;
-        let port = env.get::<String>("port")
+        let port = env
+            .get::<String>("port")
             .context("Error: Port not found")?
             .parse()
             .context("Error: Port not valid")?;
-        let database_url = env.get("database_url").context("Error: Database url not found")?;
-        Ok(Config { host, port, database_url })
+        let database_url = env
+            .get("database_url")
+            .context("Error: Database url not found")?;
+        Ok(Config {
+            host,
+            port,
+            database_url,
+        })
     }
 }
